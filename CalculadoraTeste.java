5f94f5696c1b0e54899826d1a72d568e6a698846
asbonato <asbonato@gmail.com>
2024-03-25 21:07:31 -0300

    apostila(git-java): teste da funcao somar

---
+++ CalculadoraTeste.java
@@ -0,0 +1,8 @@
+public class CalculadoraTeste {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        //testar soma
+        double soma = calc.somar(2, 3);
+        System.out.println(soma);
+    }
+}
