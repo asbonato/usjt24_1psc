c0a050af166c9724e9a01130dc3971482575d013
asbonato <asbonato@gmail.com>
2024-03-25 21:25:50 -0300

    apostila(git-java): funcao subtrair implementada

--- Calculadora.java
+++ Calculadora.java
@@ -2,4 +2,7 @@ public class Calculadora{
     public double somar(double a, double b){
         return a + b;
     }
+    public double subtrair(double a, double b){
+        return a - b;
+    }
 }
